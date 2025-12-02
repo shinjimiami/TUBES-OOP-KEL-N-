d12a08c1bb4a3f68ddcb3f937de4996d2a2d14fc
zara <zahrazahidahp@gmail.com>
2025-12-02 18:12:34 +0700

    feat: add Main, GamePanel, KeyHandler

--- app/src/main/java/nimonscooked/main/GamePanel.java
+++ app/src/main/java/nimonscooked/main/GamePanel.java
@@ -1,5 +1,80 @@
 package nimonscooked.main;
 
-public class GamePanel {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Map;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable {
+
+    final int originalTileSize = 16;
+    final int scale = 3;
+    public final int tileSize = originalTileSize * scale;
+    public final int maxScreenCol = 14;
+    public final int maxScreenRow = 10;
+    public final int screenWidth = tileSize * maxScreenCol;
+    public final int screenHeight = tileSize * maxScreenRow;
+
+    KeyHandler keyHandler = new KeyHandler();
+    Thread gameThread;
+
+    // Set player default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+//        this.addKeyListener(keyH);
+        this.setFocusable(true);
+        this.requestFocusInWindow();
+
+
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        while(gameThread != null && gameThread.isAlive()){
+
+            System.out.println("The game is running");
+
+            update();
+
+            repaint();
+        }
+
+    }
+
+    public void update(){
+
+
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setColor(Color.red);
+
+        g2d.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2d.dispose();
+    }
+
 
 }
--- app/src/main/java/nimonscooked/main/KeyHandler.java
+++ app/src/main/java/nimonscooked/main/KeyHandler.java
@@ -1,4 +1,55 @@
 package nimonscooked.main;
 
-public class KeyHandler {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+    public boolean upPressed, downPressed, leftPressed, rightPressed, enterPressed, inventoryPressed, eatPressed,
+            plantPressed, harvestPressed;
+    GamePanel gp;
+
+    public KeyHandler() {
+        this.gp = gp;
+    }
+    @Override
+    public void keyTyped(KeyEvent e){
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e){
+        int keyCode = e.getKeyCode();
+
+        if (keyCode == KeyEvent.VK_UP || keyCode == KeyEvent.VK_W) {
+            upPressed = true;
+        }
+        if (keyCode == KeyEvent.VK_DOWN || keyCode == KeyEvent.VK_S) {
+            downPressed = true;
+        }
+        if (keyCode == KeyEvent.VK_LEFT || keyCode == KeyEvent.VK_A) {
+            leftPressed = true;
+        }
+        if (keyCode == KeyEvent.VK_RIGHT || keyCode == KeyEvent.VK_D) {
+            rightPressed = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e){
+        int keyCode = e.getKeyCode();
+
+        if (keyCode == KeyEvent.VK_UP || keyCode == KeyEvent.VK_W) {
+            upPressed = true;
+        }
+        if (keyCode == KeyEvent.VK_DOWN || keyCode == KeyEvent.VK_S) {
+            downPressed = true;
+        }
+        if (keyCode == KeyEvent.VK_LEFT || keyCode == KeyEvent.VK_A) {
+            leftPressed = true;
+        }
+        if (keyCode == KeyEvent.VK_RIGHT || keyCode == KeyEvent.VK_D) {
+            rightPressed = true;
+        }
+    }
+
 }
